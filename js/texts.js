4e7dfaece23acdb5f79a664ceada4a39cc1ae927
RC4 <83184796+rc4-studio@users.noreply.github.com>
2022-05-29 20:48:39 +0100

    Update

--- js/texts.js
+++ js/texts.js
@@ -9,12 +9,13 @@
 
 
 let texts = {
-    "Quem Somos": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    "Quem Somos": "Nós somos a Associação de Estudantes Júlio Dantas, de Lagos (AEJDL), organização representativa dos alunos da Escola Secundária Júlio Dantas, de Lagos, e temos como objetivo não só representar todos os estudantes do Estabelecimento de Ensino mas também promover ao seu bem-estar, integração, defender e promover os valores fundamentais do ser humano, contribuir para a participação dos estudantes na vida escolar e na discussão dos problemas educativos, cooperar com organismos estudantis, nacionais ou estrangeiros, cujos princípios não contrariem os aqui definidos, entre muitos outros.",
     "A nossa equipa": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     events: [
-        "Apanha da batata - 15/06/2022",
-        "Apanha do malmequer - 18/06/2022",
-        "Caça ao javali - 19/06/2022",
+        "Miss e Mister: 27.05.2022",
+        "Baile de Finalistas de 9ºAno: 10.06.2022",
+        "Baile de Finalistas de 12ºAno: 27.06.2022",
+        "<a href='http://www.google.com'>Mais informação</a>",
     ]
 }
 
